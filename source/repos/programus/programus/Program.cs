1fd31fbab40cf2f7b975d30430260e5714077f3b
ExoniaQ <spect8494@gmail.com>
2022-11-09 10:25:17 +0100

    test

---
+++ source/repos/programus/programus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace programus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Joe Biden?");
+            for (int i = int.Parse(Console.ReadLine()); i > 0; i--)
+            {
+                Console.WriteLine("Joe Biden");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
